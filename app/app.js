1b8561cbedd3e43629c81367356918e9a8c17cd5
Daniel Rutz <info@danielrutz.com>
2017-07-01 15:48:26 +0200

    Made Chatting great again! Implemented fetching of Messages, fixed fetching of Channels, implemented sending of chat messages.

--- app/app.js
+++ app/app.js
@@ -2,32 +2,44 @@
 // https://stackoverflow.com/a/41753979/4375998
 
 String.prototype.hashCode = function() {
-	var hash = 5381, i = this.length
+	let hash = 5381, i = this.length
 	while(i) {
 		hash = (hash * 33) ^ this.charCodeAt(--i)
 	}
 	return hash >>> 0;
 }
 
-var app = angular.module("crappyChat", []);
-
-app.controller("chatCtrl", function($scope) {
+angular.module("crappyChat", []).controller("chatCtrl", ['$scope', '$http', function($scope, $http) {
+	$scope.endpoint = "http://[2a03:4000:6:12db:dead:beef:abad:1dea]:3000/api/chats/";
 	$scope.username = "me" // get this from login
 	$scope.apiUser = "dhbw";
 	$scope.apiPassword = "dhbw-pw";
-	$scope.channels = [ "a","b","c" ]; // do some get channels api magic here
-	$scope.messages = [{"sender": "someone", "time":"now", "text":"I hate this"},{"sender": "someoneElse", "time":"later", "text":"I hate this too"}];
+	$scope.channels = ["Lobby"];
+	$scope.messages = [];
 	$scope.users = [{"name":"someone"},{"name":"someoneElse"}, {"name": $scope.username}];
-
+	$scope.currentChannel = "Lobby";
 
 	$scope.sendMessage = function() {
-		// Do api magic here. I have no idea how, so I will just save it locally
+		// Clear line
 		var message = $scope.chatInput;
 		$scope.chatInput = null;
-		$scope.messages.push({
-			"sender":$scope.username,
-			"time": new Date().toISOString(),
-			"text": message
+
+		// POST it
+		$http.post($scope.endpoint + $scope.currentChannel,
+			JSON.stringify({
+				"message": message,
+				"user": $scope.username
+			}),
+			{
+				headers: {
+					'Authorization': 'Basic ' + btoa($scope.apiUser + ':' + $scope.apiPassword),
+					'Content-Type': 'application/json'
+				}
+			}).then(response => {
+			console.log('successfully sent message');
+			$scope.messages = response.data;
+		}, response => {
+			console.error('An error occured. Server responded: ' + response.status.toString() + ' ' + response.statusText);
 		});
 	};
 	
@@ -36,20 +48,45 @@ app.controller("chatCtrl", function($scope) {
 		
 	}
 
-	$scope.fetchChannels = function() {
-		var opts = {
-			method: 'GET',
-			headers: {
-				'Authorization': 'Basic ' + btoa($scope.apiUser, ":", $scope.apiPassword),
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}};
-		fetch("http://liebknecht.danielrutz.com:3000/api/chats/",opts)
-			//.then(response => response.json())
-			.then(data => {
-				return data;
-			})
-			.catch(err => {
-				console.error("An error ocurred:", err);
-			});
+	$scope.fetchMessages = function() {
+		$http.get($scope.endpoint + $scope.currentChannel,
+			{
+				headers: {
+					Authorization: 'Basic ' + btoa($scope.apiUser + ':' + $scope.apiPassword)
+				}
+			}).then(response => {
+			$scope.messages = response.data;
+			return response.data;
+		}, response => {
+			console.error('An error occured. Server responded: ' + response.status.toString() + ' ' + response.statusText);
+		});
 	}
-});
+
+	$scope.fetchMessages();
+	setInterval($scope.fetchMessages, 1000);
+
+	$scope.fetchChannels = function() {
+		$http.get($scope.endpoint,
+			{
+				headers: {
+					Authorization: 'Basic ' + btoa($scope.apiUser + ':' + $scope.apiPassword)
+				}
+			}).then(response => {
+			$scope.channels = response.data;
+			return response.data;
+		}, response => {
+			console.error('An error occured. Server responded: ' + response.status.toString() + ' ' + response.statusText);
+		});
+	}
+	$scope.fetchChannels();
+	setInterval($scope.fetchChannels, 5000);
+
+	$scope.setCurrentChannel = function(channel) {
+		if ($scope.channels.indexOf(channel) == -1)
+		{
+			console.error("Channel " + channel + " does not exist!");
+			return;
+		}
+		$scope.currentChannel = channel;
+	}
+}]);
